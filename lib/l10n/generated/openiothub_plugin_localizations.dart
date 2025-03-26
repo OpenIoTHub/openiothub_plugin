0e0b3581fdfe1eef0862290b0909eb5d82bc1a0d
Yu Fang <newfarry@126.com>
2025-03-26 23:22:20 +0800

    i18n git tag v1.0.2+14

--- lib/l10n/generated/openiothub_plugin_localizations.dart
+++ lib/l10n/generated/openiothub_plugin_localizations.dart
@@ -118,391 +118,391 @@ abstract class OpenIoTHubPluginLocalizations {
   /// No description provided for @select_server_for_gateway.
   ///
   /// In en, this message translates to:
-  /// **'选择本网关需要连接的服务器'**
+  /// **'Select server for gateway'**
   String get select_server_for_gateway;
 
   /// No description provided for @add_gateway_success.
   ///
   /// In en, this message translates to:
-  /// **'添加网关成功！'**
+  /// **'Add gateway success！'**
   String get add_gateway_success;
 
   /// No description provided for @login_failed.
   ///
   /// In en, this message translates to:
-  /// **'登录失败！'**
+  /// **'Login failed！'**
   String get login_failed;
 
   /// No description provided for @add_gateway_failed.
   ///
   /// In en, this message translates to:
-  /// **'添加网关失败！'**
+  /// **'Add gateway failed!'**
   String get add_gateway_failed;
 
   /// No description provided for @gateway_already_added.
   ///
   /// In en, this message translates to:
-  /// **'该网关已经被其他用户添加，请联系该网关管理员或者清空网关配置并重启网关'**
+  /// **'This gateway has been added by another user. Please contact the gateway administrator or clear the gateway configuration and restart the gateway'**
   String get gateway_already_added;
 
   /// No description provided for @confirm_gateway_connect_this_server.
   ///
   /// In en, this message translates to:
-  /// **'确认添加本网关到此服务器？'**
+  /// **'Are you sure to add this gateway to this server?'**
   String get confirm_gateway_connect_this_server;
 
   /// No description provided for @get_gateway_login_status_failed.
   ///
   /// In en, this message translates to:
-  /// **'获取网关的登录状态异常'**
+  /// **'Get abnormal login status of gateway'**
   String get get_gateway_login_status_failed;
 
   /// No description provided for @cancel.
   ///
   /// In en, this message translates to:
-  /// **'取消'**
+  /// **'Cancel'**
   String get cancel;
 
   /// No description provided for @add.
   ///
   /// In en, this message translates to:
-  /// **'添加'**
+  /// **'Add'**
   String get add;
 
   /// No description provided for @onvif_camera_manager.
   ///
   /// In en, this message translates to:
-  /// **'Onvif摄像头管理'**
+  /// **'Onvif camera manager'**
   String get onvif_camera_manager;
 
   /// No description provided for @confirm.
   ///
   /// In en, this message translates to:
-  /// **'确认'**
+  /// **'Confirm'**
   String get confirm;
 
   /// No description provided for @delete.
   ///
   /// In en, this message translates to:
-  /// **'删除'**
+  /// **'Delete'**
   String get delete;
 
   /// No description provided for @confirm_delete_onvif_device.
   ///
   /// In en, this message translates to:
-  /// **'确认删除此onvif设备？'**
+  /// **'Confirm delete onvif device?'**
   String get confirm_delete_onvif_device;
 
   /// No description provided for @onvif_camera.
   ///
   /// In en, this message translates to:
-  /// **'Onvif摄像头'**
+  /// **'Onvif camera'**
   String get onvif_camera;
 
   /// No description provided for @add_onvif_camera.
   ///
   /// In en, this message translates to:
-  /// **'添加Onvif摄像头'**
+  /// **'Add onvif camera'**
   String get add_onvif_camera;
 
   /// No description provided for @name.
   ///
   /// In en, this message translates to:
-  /// **'名称'**
+  /// **'Name'**
   String get name;
 
   /// No description provided for @custom_name.
   ///
   /// In en, this message translates to:
-  /// **'自定义名称'**
+  /// **'Custom name'**
   String get custom_name;
 
   /// No description provided for @x_addr_addr.
   ///
   /// In en, this message translates to:
-  /// **'XAddr地址'**
+  /// **'XAddr addr'**
   String get x_addr_addr;
 
   /// No description provided for @onvif_device_host_port.
   ///
   /// In en, this message translates to:
-  /// **'该机器onvif的地址:端口号'**
+  /// **'Onvif device addr:port'**
   String get onvif_device_host_port;
 
   /// No description provided for @username.
   ///
   /// In en, this message translates to:
-  /// **'用户名'**
+  /// **'Username'**
   String get username;
 
   /// No description provided for @onvif_username.
   ///
   /// In en, this message translates to:
-  /// **'onvif的用户名'**
+  /// **'Onvif username'**
   String get onvif_username;
 
   /// No description provided for @password.
   ///
   /// In en, this message translates to:
-  /// **'密码'**
+  /// **'Password'**
   String get password;
 
   /// No description provided for @onvif_password.
   ///
   /// In en, this message translates to:
-  /// **'onvif的密码'**
+  /// **'Onvif password'**
   String get onvif_password;
 
   /// No description provided for @web_browser.
   ///
   /// In en, this message translates to:
-  /// **'网页浏览器'**
+  /// **'Web browser'**
   String get web_browser;
 
   /// No description provided for @device_name.
   ///
   /// In en, this message translates to:
-  /// **'设备名称'**
+  /// **'Device name'**
   String get device_name;
 
   /// No description provided for @device_model.
   ///
   /// In en, this message translates to:
-  /// **'设备型号'**
+  /// **'Device model'**
   String get device_model;
 
   /// No description provided for @mac_addr.
   ///
   /// In en, this message translates to:
-  /// **'物理地址'**
+  /// **'MAC addr'**
   String get mac_addr;
 
   /// No description provided for @firmware_author.
   ///
   /// In en, this message translates to:
-  /// **'固件作者'**
+  /// **'Firmware author'**
   String get firmware_author;
 
   /// No description provided for @email.
   ///
   /// In en, this message translates to:
-  /// **'邮件'**
+  /// **'email'**
   String get email;
 
   /// No description provided for @home_page.
   ///
   /// In en, this message translates to:
-  /// **'主页'**
+  /// **'Home page'**
   String get home_page;
 
   /// No description provided for @firmware_repository.
   ///
   /// In en, this message translates to:
-  /// **'固件程序'**
+  /// **'Firmware repository'**
   String get firmware_repository;
 
   /// No description provided for @firmware_version.
   ///
   /// In en, this message translates to:
-  /// **'固件版本'**
+  /// **'Firmware version'**
   String get firmware_version;
 
   /// No description provided for @is_local.
   ///
   /// In en, this message translates to:
-  /// **'是否本网设备'**
+  /// **'Is local?'**
   String get is_local;
 
   /// No description provided for @yes.
   ///
   /// In en, this message translates to:
-  /// **'是'**
+  /// **'yes'**
   String get yes;
 
   /// No description provided for @no.
   ///
   /// In en, this message translates to:
-  /// **'否'**
+  /// **'no'**
   String get no;
 
   /// No description provided for @device_ip.
   ///
   /// In en, this message translates to:
-  /// **'设备地址'**
+  /// **'Device ip'**
   String get device_ip;
 
   /// No description provided for @device_port.
   ///
   /// In en, this message translates to:
-  /// **'设备端口'**
+  /// **'Device port'**
   String get device_port;
 
   /// No description provided for @device_info.
   ///
   /// In en, this message translates to:
-  /// **'设备信息'**
+  /// **'Device info'**
   String get device_info;
 
   /// No description provided for @modify_device_name.
   ///
   /// In en, this message translates to:
-  /// **'修改名称'**
+  /// **'Modify device name'**
   String get modify_device_name;
 
   /// No description provided for @input_new_device_name.
   ///
   /// In en, this message translates to:
-  /// **'请输入新的名称'**
+  /// **'Input new device name'**
   String get input_new_device_name;
 
   /// No description provided for @delete_device.
   ///
   /// In en, this message translates to:
-  /// **'删除设备'**
+  /// **'Delete device'**
   String get delete_device;
 
   /// No description provided for @confirm_delete_device.
   ///
   /// In en, this message translates to:
-  /// **'确认删除本设备?'**
+  /// **'Confirm delete device?'**
   String get confirm_delete_device;
 
   /// No description provided for @modify.
   ///
   /// In en, this message translates to:
-  /// **'修改'**
+  /// **'Modify'**
   String get modify;
 
   /// No description provided for @delete_success.
   ///
   /// In en, this message translates to:
-  /// **'删除成功!'**
+  /// **'Delete_success!'**
   String get delete_success;
 
   /// No description provided for @mdns_info.
   ///
   /// In en, this message translates to:
-  /// **'设备信息'**
+  /// **'mdns info'**
   String get mdns_info;
 
   /// No description provided for @firmware_url.
   ///
   /// In en, this message translates to:
-  /// **'固件网址'**
+  /// **'Firmware url'**
   String get firmware_url;
 
   /// No description provided for @start_ota.
   ///
   /// In en, this message translates to:
-  /// **'开始更新'**
+  /// **'Start OTA'**
   String get start_ota;
 
   /// No description provided for @temperature.
   ///
   /// In en, this message translates to:
-  /// **'温度'**
+  /// **'temperature'**
   String get temperature;
 
   /// No description provided for @humidity.
   ///
   /// In en, this message translates to:
-  /// **'湿度'**
+  /// **'humidity'**
   String get humidity;
 
   /// No description provided for @setting_name.
   ///
   /// In en, this message translates to:
-  /// **'设置名称'**
+  /// **'Setting name'**
   String get setting_name;
 
   /// No description provided for @upgrade_firmware.
   ///
   /// In en, this message translates to:
-  /// **'升级固件'**
+  /// **'Upgrade firmware'**
   String get upgrade_firmware;
 
   /// No description provided for @switch_control.
   ///
   /// In en, this message translates to:
-  /// **'开关控制'**
+  /// **'Switch control'**
   String get switch_control;
 
   /// No description provided for @on.
   ///
   /// In en, this message translates to:
-  /// **'已经开启'**
+  /// **'On'**
   String get on;
 
   /// No description provided for @off.
   ///
   /// In en, this message translates to:
-  /// **'已经关闭'**
+  /// **'Off'**
   String get off;
 
   /// No description provided for @effect.
   ///
   /// In en, this message translates to:
-  /// **'特效'**
+  /// **'Effect'**
   String get effect;
 
   /// No description provided for @switch_bottom.
   ///
   /// In en, this message translates to:
-  /// **'开关'**
+  /// **'Switch bottom'**
   String get switch_bottom;
 
   /// No description provided for @speed.
   ///
   /// In en, this message translates to:
-  /// **'速度'**
+  /// **'Speed'**
   String get speed;
 
   /// No description provided for @please_input_message.
   ///
   /// In en, this message translates to:
-  /// **'请输入消息'**
+  /// **'Please input message'**
   String get please_input_message;
 
   /// No description provided for @me.
   ///
   /// In en, this message translates to:
-  /// **'我'**
+  /// **'Me'**
   String get me;
 
   /// No description provided for @serial_port.
   ///
   /// In en, this message translates to:
-  /// **'串口'**
+  /// **'Serial port'**
   String get serial_port;
 
   /// No description provided for @please_input_linux_login_info.
   ///
   /// In en, this message translates to:
-  /// **'输入linux登录信息'**
+  /// **'Please input linux login info'**
   String get please_input_linux_login_info;
 
   /// No description provided for @linux_username.
   ///
   /// In en, this message translates to:
-  /// **'linux用户名'**
+  /// **'L(U)inux username'**
   String get linux_username;
 
   /// No description provided for @ssh_password.
   ///
   /// In en, this message translates to:
-  /// **'ssh密码'**
+  /// **'SSH password'**
   String get ssh_password;
 
   /// No description provided for @above_mentioned_ssh_password.
   ///
   /// In en, this message translates to:
-  /// **'上述linux用户密码'**
+  /// **'Above mentioned SSH password'**
   String get above_mentioned_ssh_password;
 
   /// No description provided for @connect.
   ///
   /// In en, this message translates to:
-  /// **'连接'**
+  /// **'Connect'**
   String get connect;
 }
 
--- lib/l10n/generated/openiothub_plugin_localizations_zh.dart
+++ lib/l10n/generated/openiothub_plugin_localizations_zh.dart
@@ -12,7 +12,7 @@ class OpenIoTHubPluginLocalizationsZh extends OpenIoTHubPluginLocalizations {
   String get app_title => '云亿连';
 
   @override
-  String get rdp_remote_desktop => 'RDP remote desktop';
+  String get rdp_remote_desktop => 'RDP远程桌面';
 
   @override
   String get select_server_for_gateway => '选择本网关需要连接的服务器';
@@ -219,6 +219,9 @@ class OpenIoTHubPluginLocalizationsZhCn
   String get app_title => '云亿连';
 
   @override
+  String get rdp_remote_desktop => 'RDP远程桌面';
+
+  @override
   String get select_server_for_gateway => '选择本网关需要连接的服务器';
 
   @override
@@ -256,6 +259,162 @@ class OpenIoTHubPluginLocalizationsZhCn
 
   @override
   String get confirm_delete_onvif_device => '确认删除此onvif设备？';
+
+  @override
+  String get onvif_camera => 'Onvif摄像头';
+
+  @override
+  String get add_onvif_camera => '添加Onvif摄像头';
+
+  @override
+  String get name => '名称';
+
+  @override
+  String get custom_name => '自定义名称';
+
+  @override
+  String get x_addr_addr => 'XAddr地址';
+
+  @override
+  String get onvif_device_host_port => '该机器onvif的地址:端口号';
+
+  @override
+  String get username => '用户名';
+
+  @override
+  String get onvif_username => 'onvif的用户名';
+
+  @override
+  String get password => '密码';
+
+  @override
+  String get onvif_password => 'onvif的密码';
+
+  @override
+  String get web_browser => '网页浏览器';
+
+  @override
+  String get device_name => '设备名称';
+
+  @override
+  String get device_model => '设备型号';
+
+  @override
+  String get mac_addr => '物理地址';
+
+  @override
+  String get firmware_author => '固件作者';
+
+  @override
+  String get email => '邮件';
+
+  @override
+  String get home_page => '主页';
+
+  @override
+  String get firmware_repository => '固件程序';
+
+  @override
+  String get firmware_version => '固件版本';
+
+  @override
+  String get is_local => '是否本网设备';
+
+  @override
+  String get yes => '是';
+
+  @override
+  String get no => '否';
+
+  @override
+  String get device_ip => '设备地址';
+
+  @override
+  String get device_port => '设备端口';
+
+  @override
+  String get device_info => '设备信息';
+
+  @override
+  String get modify_device_name => '修改名称';
+
+  @override
+  String get input_new_device_name => '请输入新的名称';
+
+  @override
+  String get delete_device => '删除设备';
+
+  @override
+  String get confirm_delete_device => '确认删除本设备?';
+
+  @override
+  String get modify => '修改';
+
+  @override
+  String get delete_success => '删除成功!';
+
+  @override
+  String get mdns_info => '设备信息';
+
+  @override
+  String get firmware_url => '固件网址';
+
+  @override
+  String get start_ota => '开始更新';
+
+  @override
+  String get temperature => '温度';
+
+  @override
+  String get humidity => '湿度';
+
+  @override
+  String get setting_name => '设置名称';
+
+  @override
+  String get upgrade_firmware => '升级固件';
+
+  @override
+  String get switch_control => '开关控制';
+
+  @override
+  String get on => '已经开启';
+
+  @override
+  String get off => '已经关闭';
+
+  @override
+  String get effect => '特效';
+
+  @override
+  String get switch_bottom => '开关';
+
+  @override
+  String get speed => '速度';
+
+  @override
+  String get please_input_message => '请输入消息';
+
+  @override
+  String get me => '我';
+
+  @override
+  String get serial_port => '串口';
+
+  @override
+  String get please_input_linux_login_info => '输入linux登录信息';
+
+  @override
+  String get linux_username => 'linux用户名';
+
+  @override
+  String get ssh_password => 'ssh密码';
+
+  @override
+  String get above_mentioned_ssh_password => '上述linux用户密码';
+
+  @override
+  String get connect => '连接';
 }
 
 /// The translations for Chinese, using the Han script (`zh_Hans`).
@@ -267,6 +426,9 @@ class OpenIoTHubPluginLocalizationsZhHans
   String get app_title => '云亿连';
 
   @override
+  String get rdp_remote_desktop => 'RDP remote desktop';
+
+  @override
   String get select_server_for_gateway => '选择本网关需要连接的服务器';
 
   @override
@@ -304,6 +466,162 @@ class OpenIoTHubPluginLocalizationsZhHans
 
   @override
   String get confirm_delete_onvif_device => '确认删除此onvif设备？';
+
+  @override
+  String get onvif_camera => 'Onvif摄像头';
+
+  @override
+  String get add_onvif_camera => '添加Onvif摄像头';
+
+  @override
+  String get name => '名称';
+
+  @override
+  String get custom_name => '自定义名称';
+
+  @override
+  String get x_addr_addr => 'XAddr地址';
+
+  @override
+  String get onvif_device_host_port => '该机器onvif的地址:端口号';
+
+  @override
+  String get username => '用户名';
+
+  @override
+  String get onvif_username => 'onvif的用户名';
+
+  @override
+  String get password => '密码';
+
+  @override
+  String get onvif_password => 'onvif的密码';
+
+  @override
+  String get web_browser => '网页浏览器';
+
+  @override
+  String get device_name => '设备名称';
+
+  @override
+  String get device_model => '设备型号';
+
+  @override
+  String get mac_addr => '物理地址';
+
+  @override
+  String get firmware_author => '固件作者';
+
+  @override
+  String get email => '邮件';
+
+  @override
+  String get home_page => '主页';
+
+  @override
+  String get firmware_repository => '固件程序';
+
+  @override
+  String get firmware_version => '固件版本';
+
+  @override
+  String get is_local => '是否本网设备';
+
+  @override
+  String get yes => '是';
+
+  @override
+  String get no => '否';
+
+  @override
+  String get device_ip => '设备地址';
+
+  @override
+  String get device_port => '设备端口';
+
+  @override
+  String get device_info => '设备信息';
+
+  @override
+  String get modify_device_name => '修改名称';
+
+  @override
+  String get input_new_device_name => '请输入新的名称';
+
+  @override
+  String get delete_device => '删除设备';
+
+  @override
+  String get confirm_delete_device => '确认删除本设备?';
+
+  @override
+  String get modify => '修改';
+
+  @override
+  String get delete_success => '删除成功!';
+
+  @override
+  String get mdns_info => '设备信息';
+
+  @override
+  String get firmware_url => '固件网址';
+
+  @override
+  String get start_ota => '开始更新';
+
+  @override
+  String get temperature => '温度';
+
+  @override
+  String get humidity => '湿度';
+
+  @override
+  String get setting_name => '设置名称';
+
+  @override
+  String get upgrade_firmware => '升级固件';
+
+  @override
+  String get switch_control => '开关控制';
+
+  @override
+  String get on => '已经开启';
+
+  @override
+  String get off => '已经关闭';
+
+  @override
+  String get effect => '特效';
+
+  @override
+  String get switch_bottom => '开关';
+
+  @override
+  String get speed => '速度';
+
+  @override
+  String get please_input_message => '请输入消息';
+
+  @override
+  String get me => '我';
+
+  @override
+  String get serial_port => '串口';
+
+  @override
+  String get please_input_linux_login_info => '输入linux登录信息';
+
+  @override
+  String get linux_username => 'linux用户名';
+
+  @override
+  String get ssh_password => 'ssh密码';
+
+  @override
+  String get above_mentioned_ssh_password => '上述linux用户密码';
+
+  @override
+  String get connect => '连接';
 }
 
 /// The translations for Chinese, using the Han script (`zh_Hant`).
@@ -315,6 +633,9 @@ class OpenIoTHubPluginLocalizationsZhHant
   String get app_title => '雲億連';
 
   @override
+  String get rdp_remote_desktop => 'RDP remote desktop';
+
+  @override
   String get select_server_for_gateway => '选择本网关需要连接的服务器';
 
   @override
@@ -352,6 +673,162 @@ class OpenIoTHubPluginLocalizationsZhHant
 
   @override
   String get confirm_delete_onvif_device => '确认删除此onvif设备？';
+
+  @override
+  String get onvif_camera => 'Onvif摄像头';
+
+  @override
+  String get add_onvif_camera => '添加Onvif摄像头';
+
+  @override
+  String get name => '名称';
+
+  @override
+  String get custom_name => '自定义名称';
+
+  @override
+  String get x_addr_addr => 'XAddr地址';
+
+  @override
+  String get onvif_device_host_port => '该机器onvif的地址:端口号';
+
+  @override
+  String get username => '用户名';
+
+  @override
+  String get onvif_username => 'onvif的用户名';
+
+  @override
+  String get password => '密码';
+
+  @override
+  String get onvif_password => 'onvif的密码';
+
+  @override
+  String get web_browser => '网页浏览器';
+
+  @override
+  String get device_name => '设备名称';
+
+  @override
+  String get device_model => '设备型号';
+
+  @override
+  String get mac_addr => '物理地址';
+
+  @override
+  String get firmware_author => '固件作者';
+
+  @override
+  String get email => '邮件';
+
+  @override
+  String get home_page => '主页';
+
+  @override
+  String get firmware_repository => '固件程序';
+
+  @override
+  String get firmware_version => '固件版本';
+
+  @override
+  String get is_local => '是否本网设备';
+
+  @override
+  String get yes => '是';
+
+  @override
+  String get no => '否';
+
+  @override
+  String get device_ip => '设备地址';
+
+  @override
+  String get device_port => '设备端口';
+
+  @override
+  String get device_info => '设备信息';
+
+  @override
+  String get modify_device_name => '修改名称';
+
+  @override
+  String get input_new_device_name => '请输入新的名称';
+
+  @override
+  String get delete_device => '删除设备';
+
+  @override
+  String get confirm_delete_device => '确认删除本设备?';
+
+  @override
+  String get modify => '修改';
+
+  @override
+  String get delete_success => '删除成功!';
+
+  @override
+  String get mdns_info => '设备信息';
+
+  @override
+  String get firmware_url => '固件网址';
+
+  @override
+  String get start_ota => '开始更新';
+
+  @override
+  String get temperature => '温度';
+
+  @override
+  String get humidity => '湿度';
+
+  @override
+  String get setting_name => '设置名称';
+
+  @override
+  String get upgrade_firmware => '升级固件';
+
+  @override
+  String get switch_control => '开关控制';
+
+  @override
+  String get on => '已经开启';
+
+  @override
+  String get off => '已经关闭';
+
+  @override
+  String get effect => '特效';
+
+  @override
+  String get switch_bottom => '开关';
+
+  @override
+  String get speed => '速度';
+
+  @override
+  String get please_input_message => '请输入消息';
+
+  @override
+  String get me => '我';
+
+  @override
+  String get serial_port => '串口';
+
+  @override
+  String get please_input_linux_login_info => '输入linux登录信息';
+
+  @override
+  String get linux_username => 'linux用户名';
+
+  @override
+  String get ssh_password => 'ssh密码';
+
+  @override
+  String get above_mentioned_ssh_password => '上述linux用户密码';
+
+  @override
+  String get connect => '连接';
 }
 
 /// The translations for Chinese, as used in Taiwan (`zh_TW`).
@@ -363,6 +840,9 @@ class OpenIoTHubPluginLocalizationsZhTw
   String get app_title => '雲億連';
 
   @override
+  String get rdp_remote_desktop => 'RDP remote desktop';
+
+  @override
   String get select_server_for_gateway => '选择本网关需要连接的服务器';
 
   @override
@@ -400,4 +880,160 @@ class OpenIoTHubPluginLocalizationsZhTw
 
   @override
   String get confirm_delete_onvif_device => '确认删除此onvif设备？';
+
+  @override
+  String get onvif_camera => 'Onvif摄像头';
+
+  @override
+  String get add_onvif_camera => '添加Onvif摄像头';
+
+  @override
+  String get name => '名称';
+
+  @override
+  String get custom_name => '自定义名称';
+
+  @override
+  String get x_addr_addr => 'XAddr地址';
+
+  @override
+  String get onvif_device_host_port => '该机器onvif的地址:端口号';
+
+  @override
+  String get username => '用户名';
+
+  @override
+  String get onvif_username => 'onvif的用户名';
+
+  @override
+  String get password => '密码';
+
+  @override
+  String get onvif_password => 'onvif的密码';
+
+  @override
+  String get web_browser => '网页浏览器';
+
+  @override
+  String get device_name => '设备名称';
+
+  @override
+  String get device_model => '设备型号';
+
+  @override
+  String get mac_addr => '物理地址';
+
+  @override
+  String get firmware_author => '固件作者';
+
+  @override
+  String get email => '邮件';
+
+  @override
+  String get home_page => '主页';
+
+  @override
+  String get firmware_repository => '固件程序';
+
+  @override
+  String get firmware_version => '固件版本';
+
+  @override
+  String get is_local => '是否本网设备';
+
+  @override
+  String get yes => '是';
+
+  @override
+  String get no => '否';
+
+  @override
+  String get device_ip => '设备地址';
+
+  @override
+  String get device_port => '设备端口';
+
+  @override
+  String get device_info => '设备信息';
+
+  @override
+  String get modify_device_name => '修改名称';
+
+  @override
+  String get input_new_device_name => '请输入新的名称';
+
+  @override
+  String get delete_device => '删除设备';
+
+  @override
+  String get confirm_delete_device => '确认删除本设备?';
+
+  @override
+  String get modify => '修改';
+
+  @override
+  String get delete_success => '删除成功!';
+
+  @override
+  String get mdns_info => '设备信息';
+
+  @override
+  String get firmware_url => '固件网址';
+
+  @override
+  String get start_ota => '开始更新';
+
+  @override
+  String get temperature => '温度';
+
+  @override
+  String get humidity => '湿度';
+
+  @override
+  String get setting_name => '设置名称';
+
+  @override
+  String get upgrade_firmware => '升级固件';
+
+  @override
+  String get switch_control => '开关控制';
+
+  @override
+  String get on => '已经开启';
+
+  @override
+  String get off => '已经关闭';
+
+  @override
+  String get effect => '特效';
+
+  @override
+  String get switch_bottom => '开关';
+
+  @override
+  String get speed => '速度';
+
+  @override
+  String get please_input_message => '请输入消息';
+
+  @override
+  String get me => '我';
+
+  @override
+  String get serial_port => '串口';
+
+  @override
+  String get please_input_linux_login_info => '输入linux登录信息';
+
+  @override
+  String get linux_username => 'linux用户名';
+
+  @override
+  String get ssh_password => 'ssh密码';
+
+  @override
+  String get above_mentioned_ssh_password => '上述linux用户密码';
+
+  @override
+  String get connect => '连接';
 }
